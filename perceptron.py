4aace4b6bae961f89ead4cf82fa97b21d5b37265
TAWSIF AHMED <stanfordblaze4@gmail.com>
2022-07-18 23:44:26 +0600

    Fixed Errors

--- perceptron.py
+++ perceptron.py
@@ -8,7 +8,7 @@ training_inputs = np.array([[0, 0, 1],
                             [1, 0, 1],
                             [1, 1, 1]])
 
-training_outputs = np.array([[0, 1, 1, 0]]).T
+training_outputs = np.array([[0, 0, 1, 1]]).T
 
 np.random.seed(1)
 synaptic_weights = 2 * np.random.random((3, 1)) - 1
@@ -16,7 +16,7 @@ synaptic_weights = 2 * np.random.random((3, 1)) - 1
 print('Random starting synaptic weights: ')
 print(synaptic_weights)
 
-for iteration in range(1):
+for iteration in range(20000000):
     input_layer = training_inputs
     outputs = sigmoid(np.dot(input_layer, synaptic_weights))
 
